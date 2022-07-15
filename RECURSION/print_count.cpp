706598c14dd76561434621798ae74438a200f19c
SATYAM KR SINGH <109272647+satyam-kumar94@users.noreply.github.com>
2022-07-16 00:28:22 +0530

    Add files via upload

---
+++ RECURSION/print_count.cpp
@@ -0,0 +1,24 @@
+// print counting
+
+#include <iostream>
+using namespace std;
+
+void print(int n)
+{
+    if (n == 0)
+        return;
+
+    
+    print(n - 1);
+    cout << n << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << endl;
+    print(n);
+
+    return 0;
+}
