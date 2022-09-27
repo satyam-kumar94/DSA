45c0d243231ae01484f861fbc4f7fbe6e429fa40
satyam <tech.kumarsatyam@gmail.com>
2022-09-27 14:04:03 +0530

    using recursion added

--- ARRAY/reverse_array.cpp
+++ ARRAY/reverse_array.cpp
@@ -36,3 +36,28 @@ int main(){
 
     return 0;
 }
+
+// 2nd APPROACH
+
+
+// #include <iostream>
+// using namespace std;
+// void reverse(int arr[], int start, int end)
+// {
+//     if (start >= end)
+//         return;
+//     int temp = arr[start];
+//     arr[start] = arr[end];
+//     arr[end] = temp;
+//     reverse(arr, start + 1, end - 1);
+// }
+// int main()
+// {
+//     int arr[] = {10, 20, 30, 40, 50};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     reverse(arr, 0, n - 1);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+// }
